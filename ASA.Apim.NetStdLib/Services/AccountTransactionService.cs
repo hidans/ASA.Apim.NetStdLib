e198546034597cd5adc5ad4db882c9229e310e01
Abdeslam Hidan <abdeslam@hidan.dk>
2019-03-04 13:24:58 +0100

    Add AccountTransactionService. Commit to be sync'ed between local(Y50) and Remote on GitHub

---
+++ ASA.Apim.NetStdLib/Services/AccountTransactionService.cs
@@ -0,0 +1,17 @@
+﻿using ASA.Apim.NetStdLib.Security;
+using EnvironmentAppsettings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASA.Apim.NetStdLib.Services
+{
+    public class AccountTransactionService : BaseService
+    {
+        public AccountTransactionService(ApiManagerCredentials credentials, AppSettings appSettings) : base(credentials, appSettings) { }
+
+        #region AccountTransaction
+
+        #endregion
+    }
+}
